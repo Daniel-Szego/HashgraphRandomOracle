3e9044b36419d26c6f0a9427e8b8dc63f8835da1
Daniel-Szego <szego.daniel@gmail.com>
2018-10-24 18:16:38 +0200

    initial commit

---
+++ src/OracleRandomMain.java
@@ -0,0 +1,209 @@
+
+/*
+ * This file is public domain.
+ *
+ * SWIRLDS MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF 
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+ * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. SWIRLDS SHALL NOT BE LIABLE FOR 
+ * ANY DAMAGES SUFFERED AS A RESULT OF USING, MODIFYING OR 
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ */
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Label;
+import java.nio.charset.StandardCharsets;
+
+import com.swirlds.platform.Browser;
+import com.swirlds.platform.Console;
+import com.swirlds.platform.Platform;
+import com.swirlds.platform.SwirldMain;
+import com.swirlds.platform.SwirldState;
+
+/**
+ * This HelloSwirld creates a single transaction, consisting of the string "Hello Swirld", and then goes
+ * into a busy loop (checking once a second) to see when the state gets the transaction. When it does, it
+ * prints it, too.
+ */
+public class OracleRandomMain implements SwirldMain {
+	/** the platform running this app */
+	public Platform platform;
+	/** ID number for this member */
+	public long selfId;
+	/** a console window for text output */
+	public Console console;
+	/** sleep this many milliseconds after each sync */
+	public final int sleepPeriod = 100;
+	/** a GUI window */
+	public  javax.swing.JFrame window;
+	/** a GUI window */
+	public  Integer numberOfTokens;
+	/** a GUI window */
+	public  String serviceToOffer;
+	/** Initial balances */
+	public  Integer initialBalances;
+	/** a console window for errors */
+	public Console logconsole;
+	
+
+	
+	
+	/**
+	 * This is just for debugging: it allows the app to run in Eclipse. If the config.txt exists and lists a
+	 * particular SwirldMain class as the one to run, then it can run in Eclipse (with the green triangle
+	 * icon).
+	 * 
+	 * @param args
+	 *            these are not used
+	 */
+	public static void main(String[] args) {
+		Browser.main(args);
+	}
+
+	// ///////////////////////////////////////////////////////////////////
+
+	@Override
+	public void preEvent() {
+	}
+
+	@Override
+	public void init(Platform platform, long id) {
+		this.platform = platform;
+		this.selfId = id;
+		//this.console = platform.createConsole(true); // create the window, make it visible
+		this.logconsole = platform.createConsole(true); // create the window, make it visible
+
+		String[] pars = platform.getParameters();
+		initialBalances = Integer.parseInt(pars[0]);
+		
+		platform.setAbout("Hello TimeBank v. 1.0\n"); // set the browser's "about" box
+		platform.setSleepAfterSync(sleepPeriod);
+		//this.window = platform.createWindow(true);		
+		//initWindow();
+
+	}	
+
+	protected void LogException(Exception e) {
+		logconsole.out.println(e.toString() + " " +  e.getMessage() + " ");
+	}
+	
+	protected void LogMessage(String message) {
+		logconsole.out.println(message);
+	}
+
+	
+	@Override
+	public void run() {
+		try {
+		String myName = platform.getState().getAddressBookCopy()
+				.getAddress(selfId).getSelfName();
+		serviceToOffer = platform.getState().getAddressBookCopy()
+				.getAddress(selfId).getNickname();
+
+		
+		logconsole.out.println("Time Bank Demo v.01");
+		logconsole.out.println("USER: " + myName + "");
+		logconsole.out.println("Service to offer: " + 	serviceToOffer + "");
+		logconsole.out.println("");
+		logconsole.out.println("Service DB syncronising:");
+
+		String addInitBlance = "initbalance name:" + myName + " balance: " +  initialBalances + "";
+		byte[] addInitBlancetransaction = addInitBlance.getBytes(StandardCharsets.UTF_8);
+		platform.createTransaction(addInitBlancetransaction);
+
+		String addService = "addservice name:" + myName + " service: " +  serviceToOffer + "";
+		byte[] addServicetransaction = addService.getBytes(StandardCharsets.UTF_8);
+		platform.createTransaction(addServicetransaction);
+			
+		
+/*		String lastReceivedBalance = "";
+		String lastReceivedServiceDB = "";
+		
+		boolean called = false;
+		
+		while (true) {
+			TimeBankDemoState state = (TimeBankDemoState) platform
+					.getState();
+			String receivedBalance = state.getReceivedBalances();
+			String serviceDB =  state.getReceivedServiceDB();
+			Integer differenceExistedBalance = 0;
+			Integer differenceExistedService = 0;
+			
+			if (!lastReceivedBalance.equals(receivedBalance)) {
+				lastReceivedBalance = receivedBalance;
+				logconsole.out.println("Balances: " + receivedBalance); // print all received transactions
+				differenceExistedBalance +=1;
+			}
+			if(!lastReceivedServiceDB.equals(serviceDB)) {
+				lastReceivedServiceDB = serviceDB;
+				logconsole.out.println("Services: " + serviceDB); // print all received transactions
+				differenceExistedService +=1;
+			}	
+			if (lastReceivedServiceDB.equals(serviceDB) && lastReceivedBalance.equals(receivedBalance) && !called && (differenceExistedService > 0) && (differenceExistedBalance > 0)){
+				String useService = "useservice name:" + myName + " service: " +  serviceToOffer + "";
+				byte[] useServicetransaction = useService.getBytes(StandardCharsets.UTF_8);
+				platform.createTransaction(useServicetransaction);		
+				logconsole.out.println("Calling Service " + myName + " " + serviceToOffer);
+				called = true;
+			}
+
+	
+			try {
+				Thread.sleep(sleepPeriod);
+			} catch (Exception e) {
+				LogException(e);
+			}
+		}
+		*/
+		} catch(Exception e){
+			LogException(e);
+		}
+	}
+
+	@Override
+	public SwirldState newState() {
+		return new OracleRandomState();
+	}
+	
+	
+	protected void initWindow(){
+		
+		try {
+		// label 1
+			Integer i = 0;
+			Integer j = 1;
+			j = j/i;
+			
+			Container c = window.getContentPane();
+			Label titleLbl = new Label ();		
+			Font font = titleLbl.getFont();
+			// same font but bold
+			//Font boldFont = new Font(font.getFontName(), Font.BOLD, font.getSize());
+			//titleLbl.setFont(boldFont);
+			titleLbl.setText("Timebank Demo");				
+			window.add(titleLbl, BorderLayout.AFTER_LAST_LINE);
+			
+			// label 2
+			String myName = platform.getState().getAddressBookCopy()
+					.getAddress(selfId).getSelfName();
+			Label nameLbl = new Label ();
+			nameLbl.setText("User Name" + myName);
+			window.add(nameLbl, BorderLayout.AFTER_LAST_LINE);
+			
+			// label 3
+			Label listLbl = new Label ();
+			listLbl.setText("List of services:");
+			window.add(listLbl,  BorderLayout.AFTER_LAST_LINE);
+		}
+		catch(Exception e){
+			LogException(e);
+		}
+		
+		
+	}
+
+	
+}
---
+++ src/OracleRandomState.java
@@ -0,0 +1,173 @@
+
+/*
+ * This file is public domain.
+ *
+ * SWIRLDS MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF 
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+ * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. SWIRLDS SHALL NOT BE LIABLE FOR 
+ * ANY DAMAGES SUFFERED AS A RESULT OF USING, MODIFYING OR 
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ */
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import com.swirlds.platform.Address;
+import com.swirlds.platform.AddressBook;
+import com.swirlds.platform.FCDataInputStream;
+import com.swirlds.platform.FCDataOutputStream;
+import com.swirlds.platform.FastCopyable;
+import com.swirlds.platform.Platform;
+import com.swirlds.platform.SwirldState;
+import com.swirlds.platform.Utilities;
+
+/**
+ * This holds the current state of the swirld. For this simple "hello swirld" code, each transaction is just
+ * a string, and the state is just a list of the strings in all the transactions handled so far, in the
+ * order that they were handled.
+ */
+public class OracleRandomState implements SwirldState {
+	/**
+	 * The shared state is just a list of the strings in all transactions, listed in the order received
+	 * here, which will eventually be the consensus order of the community.
+	 */
+	private List<String> strings = new ArrayList<String>();
+	/** names and addresses of all members */
+	private AddressBook addressBook;
+
+	//service database 
+ 	private Map<String, String> services = new HashMap<String, String>();
+
+	//balances 
+ 	private Map<String, Integer> balances = new HashMap<String, Integer>();
+
+	/** @return all the strings received so far from the network */
+	public synchronized List<String> getStrings() {
+		return strings;
+	}
+
+	
+	
+	/** @return all the strings received so far from the network, concatenated into one */
+	public synchronized String getReceived() {
+		return strings.toString();
+	}
+	
+	public synchronized String getReceivedBalances() {
+		   String fullState = "";
+		   
+		   Iterator it = balances.entrySet().iterator();
+		    while (it.hasNext()) {
+		        Map.Entry pair = (Map.Entry)it.next();
+		        String key = pair.getKey().toString();
+		        String value = pair.getValue().toString();
+		        fullState += key + " " + value + " ";
+		    }
+		    return fullState;	
+	}
+
+	public synchronized String getReceivedServiceDB() {
+		   String fullState = "";
+		
+		   Iterator its = services.entrySet().iterator();
+		    while (its.hasNext()) {
+		        Map.Entry pairs = (Map.Entry)its.next();
+		        String key = pairs.getKey().toString();
+		        String value = pairs.getValue().toString();
+		        fullState += key + " " + value + " ";
+		    }
+		return fullState;	
+	}
+	
+
+	/** @return the same as getReceived, so it returns the entire shared state as a single string */
+	public synchronized String toString() {
+		return strings.toString();
+	}
+
+	// ///////////////////////////////////////////////////////////////////
+
+	@Override
+	public synchronized AddressBook getAddressBookCopy() {
+		return addressBook.copy();
+	}
+
+	@Override
+	public synchronized FastCopyable copy() {
+		OracleRandomState copy = new OracleRandomState();
+		copy.copyFrom(this);
+		return copy;
+	}
+
+	@Override
+	public synchronized void copyTo(FCDataOutputStream outStream) {
+		try {
+			Utilities.writeStringArray(outStream,
+					strings.toArray(new String[0]));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public synchronized void copyFrom(FCDataInputStream inStream) {
+		try {
+			strings = new ArrayList<String>(
+					Arrays.asList(Utilities.readStringArray(inStream)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public synchronized void copyFrom(SwirldState old) {
+		strings = new ArrayList<String>(((OracleRandomState) old).strings);
+		services = new HashMap<String, String>(((OracleRandomState) old).services);
+		balances = new HashMap<String, Integer>(((OracleRandomState) old).balances);
+		addressBook = ((OracleRandomState) old).addressBook.copy();
+	}
+
+	@Override
+	public synchronized void handleTransaction(long id, boolean consensus,
+			Instant timestamp, byte[] transaction, Address address) {
+		
+		try {
+			String transactionString = new String(transaction, StandardCharsets.UTF_8);
+			
+			if (transactionString.startsWith("addservice")){
+					String name = transactionString.substring(transactionString.indexOf("name:") + 5, transactionString.indexOf("service:") - 1);
+					String serviceName = transactionString.substring(transactionString.indexOf("service:") + 9);
+					services.put(name, serviceName);									
+			}else if (transactionString.startsWith("useservice")) {
+				String name = transactionString.substring(transactionString.indexOf("name:") + 5, transactionString.indexOf("service:") - 1);
+				String serviceName = transactionString.substring(transactionString.indexOf("service:") + 9);
+				balances.put(name, balances.get(name) - 1);		
+			}else if (transactionString.startsWith("initbalance")) {
+					String name = transactionString.substring(transactionString.indexOf("name:") + 5, transactionString.indexOf("balance:") - 1);
+					Integer balance = Integer.parseInt(transactionString.substring(transactionString.indexOf("balance:") + 9));
+					balances.put(name, balance);					
+			}else{	
+				strings.add(new String(transaction, StandardCharsets.UTF_8));	
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void noMoreTransactions() {
+	}
+
+	@Override
+	public synchronized void init(Platform platform, AddressBook addressBook) {
+		this.addressBook = addressBook;
+	}
+}
